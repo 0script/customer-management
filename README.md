27ee8aca6ca242e39d5f0f2e37d0d4202cc9cfc3
0script <obmco76@gmail.com>
2022-05-31 18:57:08 +0200

    Create Model

---
+++ README.md
@@ -0,0 +1,30 @@
+#customer-management
+
+##Table of content
+* [About the project](#about-the-project)
+* [Technologies](#technologies)
+* [Features](#features)
+* [Setup](#setup)
+
+##About the project
+
+    Application that store customer information in a database .
+
+##Technologies
+
+* HTML 5 Canvas
+* CSS
+* JAVA SCRIPT
+
+##Features
+
+* CRUD operation on customer , product and order table.
+
+##Setup
+
+* Download the project and ... :
+
+```shell
+$git clone git@github.com:0script/drawing-app
+
+```
--- crm/account/models.py
+++ crm/account/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    phone=models.CharField(max_length=200,null=True)
+    email=models.EmailField( max_length=254,null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY=(
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door'),
+    )
+    name=models.CharField( max_length=50,null=True)
+    price=models.DecimalField( max_digits=5, decimal_places=2,null=True)
+    category=models.CharField( max_length=50,null=True,choices=CATEGORY)
+    description=models.CharField( max_length=250,null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+
+
+class Order(models.Model):
+    #dropdown menu for status
+    STATUS=(
+        ('Pending','Pending'),
+        ('Out for delivery','Out for delivery'),
+        ('Delivered','Delivered'),
+    )
+    #customer=
+    #product=
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    status=models.CharField( max_length=50,choices=STATUS)
