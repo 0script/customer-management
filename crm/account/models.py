8932c44e5f25e49041144ccd63053d53caec85b4
0script <obmco76@gmail.com>
2022-06-03 15:45:12 +0200

    setting models

--- crm/account/models.py
+++ crm/account/models.py
@@ -11,6 +11,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY=(
         ('Indoor','Indoor'),
@@ -18,10 +24,13 @@ class Product(models.Model):
     )
     name=models.CharField( max_length=50,null=True)
     price=models.DecimalField( max_digits=5, decimal_places=2,null=True)
+    tags=models.ForeignKey(Tag,null=True,on_delete=models.SET_NULL)
     category=models.CharField( max_length=50,null=True,choices=CATEGORY)
-    description=models.CharField( max_length=250,null=True)
+    description=models.CharField( max_length=250,null=True,blank=True)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
 
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     #dropdown menu for status
@@ -30,7 +39,9 @@ class Order(models.Model):
         ('Out for delivery','Out for delivery'),
         ('Delivered','Delivered'),
     )
-    #customer=
-    #product=
+
+    #one to many relationship
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField( max_length=50,choices=STATUS)
